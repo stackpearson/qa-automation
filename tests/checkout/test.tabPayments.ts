ae5c10cacfae631f3643e010132793035f977b87
Sawyer <sawyer.pearson92@gmail.com>
2024-10-31 17:42:18 -0600

    tab payment scenarios complete

--- tests/checkout/test.tabPayments.ts
+++ tests/checkout/test.tabPayments.ts
@@ -1,4 +1,6 @@
 import bottomButtons from '../../src/pageobjects/sharedScreens/bottomButtons.page';
+import cashPayment from '../../src/pageobjects/paymentScreens/tab/cashPayment_tab.page';
+import cartPage from '../../src/pageobjects/checkout/tab/tabCart.page';
 import categoryPage from '../../src/pageobjects/productScreens/tab/tabCategory.page';
 import checkPayment from '../../src/pageobjects/paymentScreens/tab/checkPayment_tab.page';
 import createTabPage from '../../src/pageobjects/tabs/createTab.page';
@@ -107,82 +109,215 @@ describe('Tab Payments -', () => {
         await receiptPage.tapReceiptButton('tabs')
     });
 
-    // //split payments
+    //split payments
 
-    // it('Split Payment | Cash + Credit', async () => {
-    //     await navBar.openNav();
-    //     await navDrawer.clickProshopButton();
-    //     await proshopCategoryPage.selectCategory(category);
-    //     await proshopProductPage.addToCart(productName);
-    //     await bottomButtonsPage.tapPayButton();
-    // });
+    it('Split Payment | Cash + Credit', async () => {
+        await navBar.openNav();
+        await navDrawer.clickTabButton();
+        await tabPage.tapCreateTab();
+        await createTabPage.startTab(customerDetails.email, true);
+        await tabPage.enterIntoTab(customerDetails.name);
+        await categoryPage.selectCategory(category);
+        await productPage.addToCart(productName);
+        await bottomButtons.tapPayButton('tab');
+        await paymentSelection.selectPaymentType('cash');
+        const splitOrderAmounts = await cartPage.splitOrderAmount(2);
+        await cashPayment.enterCashAmount(splitOrderAmounts.newAmount1);
+        await bottomButtons.tapPayButton('applyPayment');
 
-    // it('Split Payment | Cash + Gift Card', async () => {
-    //     await navBar.openNav();
-    //     await navDrawer.clickProshopButton();
-    //     await proshopCategoryPage.selectCategory(category);
-    //     await proshopProductPage.addToCart(productName);
-    //     await bottomButtonsPage.tapPayButton();
-    // });
+        await paymentSelection.selectPaymentType('credit');
+        await bottomButtons.tapPayButton('applyPayment');
+        await creditPayment.waitForCardReader();
+        await wdioExpect(receiptPage.orderCompleteHeader()).toBeDisplayed();
+        await receiptPage.tapReceiptButton('tabs');
+    });
 
-    // it('Split Payment | Cash + Check', async () => {
-    //     await navBar.openNav();
-    //     await navDrawer.clickProshopButton();
-    //     await proshopCategoryPage.selectCategory(category);
-    //     await proshopProductPage.addToCart(productName);
-    //     await bottomButtonsPage.tapPayButton();
-    // });
+    it('Split Payment | Cash + Gift Card', async () => {
+        await navBar.openNav();
+        await navDrawer.clickTabButton();
+        await tabPage.tapCreateTab();
+        await createTabPage.startTab(customerDetails.email, true);
+        await tabPage.enterIntoTab(customerDetails.name);
+        await categoryPage.selectCategory(category);
+        await productPage.addToCart(productName);
+        await bottomButtons.tapPayButton('tab');
+        await paymentSelection.selectPaymentType('cash');
+        const splitOrderAmounts = await cartPage.splitOrderAmount(2);
+        await cashPayment.enterCashAmount(splitOrderAmounts.newAmount1);
+        await bottomButtons.tapPayButton('applyPayment');
 
-    // it('Split Payment | Cash + Member', async () => {
-    //     await navBar.openNav();
-    //     await navDrawer.clickProshopButton();
-    //     await proshopCategoryPage.selectCategory(category);
-    //     await proshopProductPage.addToCart(productName);
-    //     await bottomButtonsPage.tapPayButton();
-    // });
+        await paymentSelection.selectPaymentType('giftcard');
+        await giftCardPayment.searchGiftCard(fullGiftCardUPC);
+        await sleep(2500);
+        await giftCardPayment.tapGiftCardResult();
+        await bottomButtons.tapPayButton('applyPayment');
+        await wdioExpect(receiptPage.orderCompleteHeader()).toBeDisplayed();
+        await receiptPage.tapReceiptButton('tabs');   
+    });
 
-    // ////
+    it('Split Payment | Cash + Check', async () => {
+        await navBar.openNav();
+        await navDrawer.clickTabButton();
+        await tabPage.tapCreateTab();
+        await createTabPage.startTab(customerDetails.email, true);
+        await tabPage.enterIntoTab(customerDetails.name);
+        await categoryPage.selectCategory(category);
+        await productPage.addToCart(productName);
+        await bottomButtons.tapPayButton('tab');
+        await paymentSelection.selectPaymentType('cash');
+        const splitOrderAmounts = await cartPage.splitOrderAmount(2);
+        await cashPayment.enterCashAmount(splitOrderAmounts.newAmount1);
+        await bottomButtons.tapPayButton('applyPayment');
 
-    // it('Split Payment | Credit + Gift Card', async () => {
-    //     await navBar.openNav();
-    //     await navDrawer.clickProshopButton();
-    //     await proshopCategoryPage.selectCategory(category);
-    //     await proshopProductPage.addToCart(productName);
-    //     await bottomButtonsPage.tapPayButton();
-    // });
+        await paymentSelection.selectPaymentType('check');
+        await checkPayment.addCheckNumber(randomCheckNumber);
+        await bottomButtons.tapPayButton('applyPayment');
+        await wdioExpect(receiptPage.orderCompleteHeader()).toBeDisplayed();
+        await receiptPage.tapReceiptButton('tabs');
+    });
 
-    // it('Split Payment | Credit + Check', async () => {
-    //     await navBar.openNav();
-    //     await navDrawer.clickProshopButton();
-    //     await proshopCategoryPage.selectCategory(category);
-    //     await proshopProductPage.addToCart(productName);
-    //     await bottomButtonsPage.tapPayButton();
-    // });
+    it('Split Payment | Cash + Member', async () => {
+        await navBar.openNav();
+        await navDrawer.clickTabButton();
+        await tabPage.tapCreateTab();
+        await createTabPage.startTab(customerDetails.email, true);
+        await tabPage.enterIntoTab(customerDetails.name);
+        await categoryPage.selectCategory(category);
+        await productPage.addToCart(productName);
+        await bottomButtons.tapPayButton('tab');
+        await paymentSelection.selectPaymentType('cash');
+        const splitOrderAmounts = await cartPage.splitOrderAmount(2);
+        await cashPayment.enterCashAmount(splitOrderAmounts.newAmount1);
+        await bottomButtons.tapPayButton('applyPayment');
 
-    // it('Split Payment | Credit + Member', async () => {
-    //     await navBar.openNav();
-    //     await navDrawer.clickProshopButton();
-    //     await proshopCategoryPage.selectCategory(category);
-    //     await proshopProductPage.addToCart(productName);
-    //     await bottomButtonsPage.tapPayButton();
-    // });
+        await paymentSelection.selectPaymentType('customercharge');
+        await memberPayment.searcMembers(customerDetails.email);
+        await sleep(2500);
+        await memberPayment.selectFirstMemberResult();
+        await bottomButtons.tapPayButton('applyPayment');
+        await wdioExpect(receiptPage.orderCompleteHeader()).toBeDisplayed();
+        await receiptPage.tapReceiptButton('tabs')
+    });
 
-    // ////
+    it('Split Payment | Credit + Gift Card', async () => {
+        await navBar.openNav();
+        await navDrawer.clickTabButton();
+        await tabPage.tapCreateTab();
+        await createTabPage.startTab(customerDetails.email, true);
+        await tabPage.enterIntoTab(customerDetails.name);
+        await categoryPage.selectCategory(category)
+        await productPage.addToCart(productName);
+        await bottomButtons.tapPayButton('tab');
+        await paymentSelection.selectPaymentType('credit');
+        const splitOrderAmounts = await cartPage.splitOrderAmount(2);
+        await creditPayment.enterChargeAmount(splitOrderAmounts.newAmount1)
+        await bottomButtons.tapPayButton('applyPayment');
+        await creditPayment.waitForCardReader();
 
-    // it('Split Payment | Gift Card + Check', async () => {
-    //     await navBar.openNav();
-    //     await navDrawer.clickProshopButton();
-    //     await proshopCategoryPage.selectCategory(category);
-    //     await proshopProductPage.addToCart(productName);
-    //     await bottomButtonsPage.tapPayButton();
-    // });
+        await paymentSelection.selectPaymentType('giftcard');
+        await giftCardPayment.searchGiftCard(fullGiftCardUPC);
+        await sleep(2500);
+        await giftCardPayment.tapGiftCardResult();
+        await bottomButtons.tapPayButton('applyPayment');
+        await wdioExpect(receiptPage.orderCompleteHeader()).toBeDisplayed();
+        await receiptPage.tapReceiptButton('tabs');
+    });
 
+    it('Split Payment | Credit + Check', async () => {
+        await navBar.openNav();
+        await navDrawer.clickTabButton();
+        await tabPage.tapCreateTab();
+        await createTabPage.startTab(customerDetails.email, true);
+        await tabPage.enterIntoTab(customerDetails.name);
+        await categoryPage.selectCategory(category)
+        await productPage.addToCart(productName);
+        await bottomButtons.tapPayButton('tab');
+        await paymentSelection.selectPaymentType('credit');
+        const splitOrderAmounts = await cartPage.splitOrderAmount(2);
+        await creditPayment.enterChargeAmount(splitOrderAmounts.newAmount1)
+        await bottomButtons.tapPayButton('applyPayment');
+        await creditPayment.waitForCardReader();
+
+        await paymentSelection.selectPaymentType('check');
+        await checkPayment.addCheckNumber(randomCheckNumber);
+        await bottomButtons.tapPayButton('applyPayment');
+        await wdioExpect(receiptPage.orderCompleteHeader()).toBeDisplayed();
+        await receiptPage.tapReceiptButton('tabs');
+    });
+
+    it('Split Payment | Credit + Member', async () => {
+        await navBar.openNav();
+        await navDrawer.clickTabButton();
+        await tabPage.tapCreateTab();
+        await createTabPage.startTab(customerDetails.email, true);
+        await tabPage.enterIntoTab(customerDetails.name);
+        await categoryPage.selectCategory(category)
+        await productPage.addToCart(productName);
+        await bottomButtons.tapPayButton('tab');
+        await paymentSelection.selectPaymentType('credit');
+        const splitOrderAmounts = await cartPage.splitOrderAmount(2);
+        await creditPayment.enterChargeAmount(splitOrderAmounts.newAmount1)
+        await bottomButtons.tapPayButton('applyPayment');
+        await creditPayment.waitForCardReader();
+        
+        await paymentSelection.selectPaymentType('customercharge');
+        await memberPayment.searcMembers(customerDetails.email);
+        await sleep(2500);
+        await memberPayment.selectFirstMemberResult();
+        await bottomButtons.tapPayButton('applyPayment');
+        await wdioExpect(receiptPage.orderCompleteHeader()).toBeDisplayed();
+        await receiptPage.tapReceiptButton('tabs')
+    });
+
+    it('Split Payment | Gift Card + Check', async () => {
+        await navBar.openNav();
+        await navDrawer.clickTabButton();
+        await tabPage.tapCreateTab();
+        await createTabPage.startTab(customerDetails.email, true);
+        await tabPage.enterIntoTab(customerDetails.name);
+        await categoryPage.selectCategory(category);
+        await productPage.addToCart(productName);
+        await bottomButtons.tapPayButton('tab');
+        await paymentSelection.selectPaymentType('check');
+        const splitOrderAmounts = await cartPage.splitOrderAmount(2);
+        await checkPayment.addCheckNumber(randomCheckNumber);
+        await checkPayment.enterCheckAmount(splitOrderAmounts.newAmount1);
+        await bottomButtons.tapPayButton('applyPayment');
+
+        await paymentSelection.selectPaymentType('giftcard');
+        await giftCardPayment.searchGiftCard(fullGiftCardUPC);
+        await sleep(2500);
+        await giftCardPayment.tapGiftCardResult();
+        await bottomButtons.tapPayButton('applyPayment');
+        await wdioExpect(receiptPage.orderCompleteHeader()).toBeDisplayed();
+        await receiptPage.tapReceiptButton('tabs');
+    });
+
+    //swapping from member pay to GC crashes the app - awaiting fix before uncommenting this test case
     // it('Split Payment | Gift Card + Member', async () => {
     //     await navBar.openNav();
-    //     await navDrawer.clickProshopButton();
-    //     await proshopCategoryPage.selectCategory(category);
-    //     await proshopProductPage.addToCart(productName);
-    //     await bottomButtonsPage.tapPayButton();
+    //     await navDrawer.clickTabButton();
+    //     await tabPage.tapCreateTab();
+    //     await createTabPage.startTab(customerDetails.email, true);
+    //     await tabPage.enterIntoTab(customerDetails.name);
+    //     await categoryPage.selectCategory(category)
+    //     await productPage.addToCart(productName);
+    //     await bottomButtons.tapPayButton('tab');
+    //     await paymentSelection.selectPaymentType('customercharge');
+    //     const splitOrderAmounts = await cartPage.splitOrderAmount(2);
+    //     await memberPayment.searcMembers(customerDetails.email);
+    //     await sleep(2500);
+    //     await memberPayment.selectFirstMemberResult();
+    //     await memberPayment.enterMemberChargeAmount(splitOrderAmounts.newAmount1);
+    //     await bottomButtons.tapPayButton('applyPayment');
+
+    //     await paymentSelection.selectPaymentType('giftcard');
+    //     await giftCardPayment.searchGiftCard(fullGiftCardUPC);
+    //     await sleep(2500);
+    //     await giftCardPayment.tapGiftCardResult();
+    //     await bottomButtons.tapPayButton('applyPayment');
+    //     await wdioExpect(receiptPage.orderCompleteHeader()).toBeDisplayed();
+    //     await receiptPage.tapReceiptButton('tabs');
     // });
 
 })
